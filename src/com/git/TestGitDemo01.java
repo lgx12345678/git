1788e41187adb4e395f7dbb570a81d8149bfb414
boniu <boniu>
2021-05-14 19:55:37 +0800

    我修改了TestGitDemo01，将项目提交并且推送到远程仓库

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -4,6 +4,6 @@ public class TestGitDemo01 {
     public static void main(String[] args) {
 
         System.out.println("这是TestGitDemo01");
-        System.out.println("我修改了TestGitDemo01");
+        System.out.println("我修改过了TestGitDemo01");
     }
 }
